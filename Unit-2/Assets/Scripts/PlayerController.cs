84b45a34b7aaaf2ada851d378cda61fe642a8170
Hamza Albaş <hamza.albas@hotmail.com>
2023-03-09 14:48:53 +0300

    Lesson 2.1 completed.

---
+++ Unit-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    public float speed;
+    public float maxPos;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(transform.position.x > maxPos)
+        {
+            transform.position = new Vector3(maxPos, 0, 0);
+        }
+        if(transform.position.x < -maxPos)
+        {
+            transform.position = new Vector3(-maxPos, 0, 0);
+        }
+
+        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+    }
+}
