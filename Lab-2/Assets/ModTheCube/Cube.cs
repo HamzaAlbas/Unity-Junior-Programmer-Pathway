6873c35f6f67858d78a0c0d56cae941c4751d6c1
Hamza Albaş <hamza.albas@hotmail.com>
2023-03-10 14:01:17 +0300

    Modifications completed.

--- Lab-2/Assets/ModTheCube/Cube.cs
+++ Lab-2/Assets/ModTheCube/Cube.cs
@@ -5,19 +5,113 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    private Material material;
+    private float direction = 1;
+
+    private float randomR;
+    private float randomG;
+    private float randomB;
+    private float randomA;
+
+    private float randomX;
+    private float randomY;
+    private float randomZ;
+
+    private float randomRot;
+    private float randomRotSpeed;
+
+    private int randomIndex;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
+        //transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material = Renderer.material;
+        randomIndex = Random.Range(0, 4);
+        Debug.Log("Index: " + randomIndex);
+
+        InvokeRepeating("RandomColor", 1, 1);
+        InvokeRepeating("RandomScale", 1, 1);
+        InvokeRepeating("RandomRotation", 1, 1);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        if (randomIndex == 0)
+        {
+            ChangeColor();
+        }
+        else if (randomIndex == 1)
+        {
+            ChangePosition();
+        }
+        else if (randomIndex == 2)
+        {
+            ChangeScale();
+        }
+        else if (randomIndex == 3)
+        {
+            ChangeRotation();
+        }
+        else if (randomIndex == 4)
+        {
+            ChangeColor();
+            ChangePosition();
+            ChangeRotation();
+            ChangeScale();
+        }
     }
+
+    private void ChangeColor()
+    {
+        material.color = new Color(randomR, randomG, randomB, randomA);
+    }
+
+    private void ChangePosition()
+    {
+
+        transform.Translate(5 * Time.deltaTime * new Vector3(0, 0, direction));
+
+        if (transform.position.z >= 10)
+        {
+            direction = -1;
+        }
+        else if (transform.position.z <= -10)
+        {
+            direction = 1;
+        }
+    }
+
+    private void ChangeScale()
+    {
+        transform.localScale = new Vector3(randomX, randomY, randomZ);
+    }
+
+    private void ChangeRotation()
+    {
+        transform.Rotate(randomRot * Time.deltaTime * randomRotSpeed, 0.0f, 0.0f);
+    }
+
+    private void RandomColor()
+    {
+        randomR = Random.Range(0, 1.0f);
+        randomG = Random.Range(0, 1.0f);
+        randomB = Random.Range(0, 1.0f);
+        randomA = Random.Range(0, 1.0f);
+    }
+
+    private void RandomScale()
+    {
+        randomX = Random.Range(0.5f, 5f);
+        randomY = Random.Range(0.5f, 5f);
+        randomZ = Random.Range(0.5f, 5f);
+    }
+
+    private void RandomRotation()
+    {
+        randomRot = Random.Range(0f, 180f);
+        randomRotSpeed = Random.Range(1f, 10f);
+    }
+
+
 }
